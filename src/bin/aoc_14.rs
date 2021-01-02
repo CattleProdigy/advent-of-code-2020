d2ea9152fc3d928dcc4ebe009c8f5709389fc209
CattleProdigy <paulschroeder90@gmail.com>
2021-01-02 14:11:13 -0500

    day 14: part2
    - Used a hash map to simulate memory space and did some bit-twiddling
    to generate permutation for the floating bits.
    - I gave up on the slick bit masking approach.
    - I tried this a few weeks ago and it was running slow, not sure what
    the problem was. This only takes 50ms.

--- src/bin/aoc_14.rs
+++ src/bin/aoc_14.rs
@@ -4,208 +4,93 @@ use std::fs;
 
 #[derive(Debug)]
 struct ProgramState {
-    old_mask: usize,
-    new_mask: usize,
+    addr_mask: usize,
+    float_mask: usize,
     memory: HashMap<usize, usize>,
 }
 
 #[derive(Debug, Clone)]
 enum Instruction {
-    Mask { old_mask: usize, new_mask: usize },
+    Mask { addr_mask: usize, float_mask: usize },
     Assignment { addr: usize, value: usize },
 }
-// fn run_p2(insts: &Vec<Instruction>) -> ProgramState {
-//     let mut ps = ProgramState {
-//         old_mask: 0,
-//         new_mask: 0,
-//         memory: HashMap::new(),
-//     };
-//
-//     println!("{:?}", ps);
-//     for i in insts.iter() {
-//         match i {
-//             Instruction::Mask { old_mask, new_mask } => {
-//                 ps.old_mask = *old_mask;
-//                 ps.new_mask = *new_mask;
-//             }
-//             Instruction::Assignment { addr, value } => {
-//                 let mut x_bits: Vec<usize> = Vec::new();
-//                 {
-//                     let mut x_ones = ps.old_mask;
-//                     for i in 0..36 {
-//                         if x_ones & 1 == 1 {
-//                             x_bits.push(i);
-//                         }
-//                         x_ones = x_ones >> 1;
-//                     }
-//                 }
-//
-//                 let base_addr = (addr | ps.new_mask) & (!ps.old_mask);
-//
-//                 let floating_gen_max = 1 << x_bits.len();
-//
-//                 println!("assign {} mem addrs", floating_gen_max);
-//                 for fg in 0..floating_gen_max {
-//                     let mut fg_shifter = fg;
-//                     let mut fg_bits: Vec<usize> = Vec::new();
-//                     for _ in 0..x_bits.len() {
-//                         fg_bits.push(fg_shifter & 1);
-//                         fg_shifter = fg_shifter >> 1;
-//                     }
-//
-//                     let mut new_addr = base_addr;
-//                     for (bit_pos, bit_val) in x_bits.iter().zip(fg_bits.iter()) {
-//                         new_addr = (new_addr & !(1 << bit_pos)) | (bit_val << bit_pos);
-//                     }
-//
-//                     let mem_val = ps.memory.entry(new_addr).or_insert(0);
-//                     *mem_val = *value;
-//                 }
-//             }
-//         }
-//         // println!("{:?}", ps);
-//     }
-//
-//     ps
-// }
 
 fn run_p2(insts: &Vec<Instruction>) -> usize {
     let mut writes: Vec<(usize, usize, usize)> = Vec::new();
     let mut ps = ProgramState {
-        old_mask: 0,
-        new_mask: 0,
+        addr_mask: 0,
+        float_mask: 0,
         memory: HashMap::new(),
     };
 
     for i in insts.iter() {
         match i {
-            Instruction::Mask { old_mask, new_mask } => {
-                ps.old_mask = *old_mask;
-                ps.new_mask = *new_mask;
+            Instruction::Mask {
+                addr_mask,
+                float_mask,
+            } => {
+                ps.addr_mask = *addr_mask;
+                ps.float_mask = *float_mask;
             }
             Instruction::Assignment { addr, value } => {
-                writes.push((*value, ps.old_mask, ((addr & !ps.old_mask) | ps.new_mask)));
+                writes.push((
+                    *value,
+                    ps.float_mask,
+                    (addr & !ps.float_mask) | ps.addr_mask,
+                ));
             }
         }
     }
 
-    // 1: 0b00X1 fl:0010 fx:0001
-    // 2: 0b00XX fl:0011 fx:0000
-    //  400
-    //
-    //
-
-    // 1: 1     0bX0X1 1010 0001
-    // 2: 100   0b0XXX 0111 0000
-    // 3: 10000 0b01XX 0011 0100
-    //
-    // 1) 4 * 10000 = 40000
-    // 2) new effective addr = 0b00XX
-    //    2^2 * 100 = 400;
-    // 3) 4 possible
-    //     new effective addr = 0b10X1
-    //     2^1 * 1 = 2
-    //     40402
-
-    println!("writes {:?}", writes);
-    // start from the back
-    // mask off all the values get overwritten later,
-    // get remaining floating bits, add assigned value * 2^floating bits
-    //
-    // (1) if any fixed bits differ, they don't overlap, no cancellation
-    // (2) if a bit is floating and later a fixed bit writes, floating becomes flipped fix
-    // (2) if a bit is fixed and later a floating writes, floating becomes flipped fix
-    let mut memory_sum: usize = 0;
-    for (i, write) in writes.iter().enumerate().rev() {
-        let value = write.0;
-        let mut floating_mask = write.1;
-        let mut fixed_mask = write.2;
-        let mut overwritten = false;
-        println!("{}", value);
-        println!("fl: {:036b}", floating_mask);
-        println!("fx: {:036b}", fixed_mask);
-        let floating_bits = floating_mask.count_ones();
-        println!(" b: {}", floating_bits);
-        println!("=======");
-
-        for later_write in (&writes[i + 1..]).iter() {
-            let later_floating_mask = later_write.1;
-            let later_fixed_mask = later_write.2;
-
-            let tsmask = 0xfffffff000000000;
-            if floating_mask & tsmask > 0
-                || fixed_mask & tsmask > 0
-                || later_floating_mask & tsmask > 0
-                || later_fixed_mask & tsmask > 0
-            {
-                panic!("something happened");
-            }
-            println!("\t v: {}", later_write.0);
-            println!("\tfl: {:036b}", later_floating_mask);
-            println!("\tfx: {:036b}", later_fixed_mask);
-
-            // skip there's no overlap
-            if (!later_floating_mask & !floating_mask & later_fixed_mask)
-                != (!later_floating_mask & !floating_mask & fixed_mask)
-            {
-                println!("skipping");
-                continue;
-            }
-
-            // if new floating mask is subset of later floating mask
-            // then we're overwritten entirely
-
-            fixed_mask |= (!later_floating_mask & floating_mask) & (!later_fixed_mask);
-            floating_mask &= !(!later_floating_mask & floating_mask);
-            println!("\tnl: {:036b}", floating_mask);
-            println!("\tnx: {:036b}", fixed_mask);
-            let floating_bits = floating_mask.count_ones();
-            println!("\t b: {}", floating_bits);
-
-            if (!later_floating_mask & !floating_mask & later_fixed_mask)
-                == (!later_floating_mask & !floating_mask & fixed_mask)
-            {
-                if !later_floating_mask & floating_mask == 0 {
-                    println!("overwritten");
-                    overwritten = true;
+    for (v, f, addr) in writes.iter() {
+        let mut floating_bits: Vec<usize> = Vec::new();
+        {
+            let mut f_shift = *f;
+            for i in 0..64 {
+                if f_shift == 0 {
                     break;
                 }
+                if f_shift & 1 == 1 {
+                    floating_bits.push(i);
+                }
+
+                f_shift = f_shift >> 1;
             }
         }
 
-        if overwritten {
-            continue;
-        }
-        let floating_bits = floating_mask.count_ones();
+        let num_perms = 1 << floating_bits.len();
+        for i in 0..num_perms {
+            let mut set_bits: Vec<usize> = Vec::new();
+            let mut clear_bits: Vec<usize> = Vec::new();
+            {
+                let mut i_shift = i;
+                for i in 0..floating_bits.len() {
+                    if i_shift & 1 == 1 {
+                        set_bits.push(i);
+                    } else if i_shift & 1 == 0 {
+                        clear_bits.push(i);
+                    }
 
-        memory_sum += (value) * (1 << floating_bits);
+                    i_shift = i_shift >> 1;
+                }
+            }
+            let mut addr_mut = *addr;
+            for s in set_bits {
+                addr_mut |= 1 << floating_bits[s];
+            }
+            for c in clear_bits {
+                addr_mut &= !(1 << floating_bits[c]);
+            }
+            ps.memory.insert(addr_mut, *v);
+        }
     }
 
-    memory_sum
-}
-
-fn run(insts: &Vec<Instruction>) -> ProgramState {
-    let mut ps = ProgramState {
-        old_mask: 0,
-        new_mask: 0,
-        memory: HashMap::new(),
-    };
-
-    for i in insts.iter() {
-        match i {
-            Instruction::Mask { old_mask, new_mask } => {
-                ps.old_mask = *old_mask;
-                ps.new_mask = *new_mask;
-            }
-            Instruction::Assignment { addr, value } => {
-                let mem_val = ps.memory.entry(*addr).or_insert(0);
-                let new_val = (ps.old_mask & value) | (ps.new_mask);
-                *mem_val = new_val;
-            }
-        }
+    let mut mem_sum = 0;
+    for (_, v) in ps.memory.iter() {
+        mem_sum += v;
     }
 
-    ps
+    mem_sum
 }
 
 fn parse(input_str: &str) -> Vec<Instruction> {
@@ -228,13 +113,13 @@ fn parse(input_str: &str) -> Vec<Instruction> {
             } else if l.starts_with("mask") {
                 let equals_sign = l.find('=').unwrap();
                 let mask_slice = &l[equals_sign + 2..];
-                let old_mask_str = mask_slice.replace('1', "0").replace('X', "1");
-                let old_mask = usize::from_str_radix(&old_mask_str, 2).unwrap();
-                let new_mask_str = mask_slice.replace('X', "0");
-                let new_mask = usize::from_str_radix(&new_mask_str, 2).unwrap();
+                let float_mask_str = mask_slice.replace('1', "0").replace('X', "1");
+                let float_mask = usize::from_str_radix(&float_mask_str, 2).unwrap();
+                let addr_mask_str = mask_slice.replace('X', "0");
+                let addr_mask = usize::from_str_radix(&addr_mask_str, 2).unwrap();
                 Instruction::Mask {
-                    old_mask: old_mask,
-                    new_mask: new_mask,
+                    addr_mask: addr_mask,
+                    float_mask: float_mask,
                 }
             } else {
                 panic!("Unknown opcode: {}", l);
@@ -251,10 +136,6 @@ fn main() {
 
     let file_contents = fs::read_to_string(&args[1]).expect("couldn't read the file");
     let insts = parse(&file_contents);
-
-    let end_ps = run(&insts);
-    let mem_sum_p1 = end_ps.memory.values().sum::<usize>();
-    println!("p1: {}", mem_sum_p1);
 
     let mem_sum_p2 = run_p2(&insts);
     println!("p2: {}", mem_sum_p2);
